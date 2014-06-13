0f33c53c086e1a3875914dff675024347b10263b
Alexis Toulotte <al@alweb.org>
2014-06-13 14:00:12 +1100

    improved AssociationCallbacks.version specs

--- spec/association_callbacks_spec.rb
+++ spec/association_callbacks_spec.rb
@@ -4,17 +4,14 @@ describe AssociationCallbacks do
 
   describe '.version' do
 
-    it 'is a string' do
-      expect(AssociationCallbacks.version).to be_a(String)
-    end
+    subject { AssociationCallbacks.version }
 
-    it 'is with correct format' do
-      expect(AssociationCallbacks.version).to match(/^\d+\.\d+(\.\d+)?$/)
-    end
+    it { should be_a(String) }
+    it { should match(/^\d+\.\d+(\.\d+)?$/) }
 
     it 'is freezed' do
       expect {
-        AssociationCallbacks.version.gsub!('.', '#')
+        subject.gsub!('.', '#')
       }.to raise_error(/can't modify frozen string/i)
     end
 
